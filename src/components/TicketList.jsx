7956b3b10895c156273ad510903d7a7a746eaee1
Margaret Berry <codeberry1@gmail.com>
2017-12-02 12:27:02 -0800

    set up server

--- src/components/TicketList.jsx
+++ src/components/TicketList.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import Ticket from "./Ticket";
 
 function TicketList(props){
   return (
     <Ticket
-      location="3a" 
+      location="3a"
       names="Thato and Haley"
       issue="Firebase won't save record"/>
   );
